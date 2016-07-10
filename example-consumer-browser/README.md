fbe2b17d4fc87b6fdf6e4ee7e9d3c6a2f2d0faa1
Corey Ferguson <corey.t.ferguson@gmail.com>
2016-07-10 16:07:02 -0700

    documentation and http

---
+++ example-consumer-browser/README.md
@@ -0,0 +1,11 @@
+
+# example-consumer-browser
+
+## Summary
+
+Examples of how to consume Iso-Lib in the browser.
+
+## Examples
+
+- `static/index-amd.html`: Consume with RequireJS.
+- `static/index-this.html`: Consume by adding to global context.
--- example-consumer-browser/server/index.js
+++ example-consumer-browser/server/index.js
@@ -7,7 +7,7 @@ const path = require('path');
 // external dependencies
 const open = require('open');
 const express = require('express');
-const https = require('https');
+const http = require('http');
 
 // config
 const port = 3000;
@@ -21,12 +21,8 @@ app.use('/node_modules', express.static(path.join(__dirname, '../node_modules'))
 app.use(express.static(path.join(__dirname, '../static')));
 
 // start express
-const options = {
-	key: fs.readFileSync(path.join(__dirname, 'privatekey.pem')),
-	cert: fs.readFileSync(path.join(__dirname, 'certificate.pem'))
-};
-const httpsServer = https.createServer(options, app).listen(port, () => {
+const httpsServer = http.createServer(app).listen(port, () => {
 	console.info('Listening on port %d...', httpsServer.address().port)
-  open('https://localhost:' + port + '/index-this.html', preferredBrowser);
-	open('https://localhost:' + port + '/index-amd.html', preferredBrowser);
+  open('http://localhost:' + port + '/index-this.html', preferredBrowser);
+	open('http://localhost:' + port + '/index-amd.html', preferredBrowser);
 });
